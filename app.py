7bb269e762259eaa8360fc3f552ae28c44dc5f16
devopsengineer-ershad <ershadalams150@gmail.com>
2025-11-25 12:05:48 +0530

    Change database connection string password
    Updated the database connection string with a new password.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # connection_string = os.getenv("CONNECTION_STRING")
-connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:ershapp-dev-eastus-sqlsrv.database.windows.net,1433;Database=todo-database;Uid=sqladmintodo;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:ershapp-dev-eastus-sqlsrv.database.windows.net,1433;Database=todo-database;Uid=sqladmintodo;Pwd=P@ssword123!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 app = FastAPI()
 
 # Configure CORSMiddleware to allow all origins (disable CORS for development)
